21a4ff981b21cc794c56f69d571254410e27effc
Murilo Santana <murilo.sntn10@gmail.com>
2022-09-26 10:44:07 -1000

    FindById

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,19 +1,22 @@
 package aplication;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
+
 
 public class Program {
 
+		
 	public static void main(String[] args) {
 		
-		Department dpto = new Department(1,"Contabilidade");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller sel = new Seller(1,"Bob","bob@example.com", new Date(), 3000.00 , dpto);
+		Seller seller = sellerDao.findById(2);
 		
-		System.out.println(sel);
+		System.out.println(seller);
+		
+
 
 	}
 
