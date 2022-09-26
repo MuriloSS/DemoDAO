f122b619567d527a616c5b689dc423d8bd22b42f
Murilo Santana <murilo.sntn10@gmail.com>
2022-09-26 03:00:20 -1000

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,9 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,9 @@ public class Program {
 		
 		Department dpto = new Department(1,"Contabilidade");
 		
-		System.out.println(dpto);
+		Seller sel = new Seller(1,"Bob","bob@example.com", new Date(), 3000.00 , dpto);
+		
+		System.out.println(sel);
 
 	}
 
